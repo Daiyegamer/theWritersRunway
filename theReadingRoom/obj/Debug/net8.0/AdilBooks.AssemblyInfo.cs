c07f5de8b0a3045bab3dfdd31ca3ffb473e66656
Genevieve <awavieve@gmail.com>
2025-03-28 17:22:40 -0400

    No changes

--- theReadingRoom/obj/Debug/net8.0/AdilBooks.AssemblyInfo.cs
+++ theReadingRoom/obj/Debug/net8.0/AdilBooks.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdilBooks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b43677d6a9f0add4715f2c86ae16df3135133b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7813b688a1ca2a6a2905b312868cef481e6656e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdilBooks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdilBooks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
